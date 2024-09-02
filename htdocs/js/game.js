2ecbf7da68f4f4bd4ef788b3c85292c9bf8b002c
Marie-Astrid Buelens <mabuelens@gmail.com>
2024-09-02 12:04:13 +0200

    first commit

---
+++ htdocs/js/game.js
@@ -0,0 +1,62 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 640,
+    physics: {
+        default: 'arcade'
+    },
+    scene: {
+        preload: preload,
+        create: create
+    },
+    autoCenter: true
+};
+
+let game = new Phaser.Game(config);
+let state;
+let stateText;
+
+function preload() {
+    this.load.image('background', '/assets/Sprites/background.png');
+    this.load.image('button', '/assets/Sprites/block.png');
+}
+
+function create() {
+    let backImage = this.add.image(0, 0, 'background');
+    backImage.setOrigin(0, 0);
+    backImage.setScale(0.5);
+    stateText = this.add.text(150, 80, "", 
+        { fontFamily: 'Arial', fontSize: 18, color: '#00ff00' });
+    let buttonImage = this.add.image(100, 100, 'button').setInteractive();
+    buttonImage.setScale(0.3);
+    buttonImage.on('pointerdown', feed);
+    setInterval(updateState, 10000);
+}
+
+function updateState() {
+    // envoyer la requête GET vers le serveur
+    fetch('http://localhost:8000/state') 
+    .then(function (response) { 
+        return response.json(); 
+        }) 
+            .then(function (stateFromServer) { 
+                stateText.text = stateFromServer.hungry; 
+            }) 
+    .catch(function (err) { 
+        console.log("Something went wrong!", err); 
+        });
+}
+
+function feed() {
+    // envoyer la requête GET vers le serveur
+    fetch('http://localhost:8000/feed') 
+    .then(function (response) { 
+        return response.json(); 
+        }) 
+            .then(function (stateFromServer) { 
+                stateText.text = stateFromServer.hungry; 
+            }) 
+    .catch(function (err) { 
+        console.log("Something went wrong!", err); 
+        });
+}
