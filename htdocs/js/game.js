cbfd61a2c2dc6d6d6aa7d80bd6cbd2e1919d0db4
Marie-Astrid Buelens <mabuelens@gmail.com>
2024-09-04 13:53:49 +0200

    fonction death

--- htdocs/js/game.js
+++ htdocs/js/game.js
@@ -9,7 +9,10 @@ let config = {
         preload: preload,
         create: create
     },
-    autoCenter: true
+    autoCenter: true,
+    audio: {
+        disableWebAudio: true
+    }
 };
 
 let game = new Phaser.Game(config);
@@ -19,21 +22,35 @@ let stateTextSleep;
 let stateTextPlay;
 let stateTextCounter;
 let counter;
+let deathImage;
+let buttonStart;
 let buttonImageHunger;
 let buttonImageSleep;
 let buttonImagePlay;
 let timeToWait=5000;
+let backgroundMusic;
+let musicOn = false;
 
 
 function preload() {
-    this.load.image('background', '/assets/Sprites/background.png');
+    this.load.image('background', '/assets/Sprites/background2.jpg');
+    this.load.image('death', '/assets/Sprites/deathTemp.jpg');
     this.load.image('button', '/assets/Sprites/block.png');
+    this.load.audio('backgroundMusic', '/assets/Sound/Alexander Ehlers - Flags.mp3')
 }
 
 function create() {
     let backImage = this.add.image(0, 0, 'background');
     backImage.setOrigin(0, 0);
-    backImage.setScale(0.55);
+    backImage.setScale(0.9);
+
+    deathImage = this.add.image(0, 0, 'death');
+    deathImage.setOrigin(0, 0);
+    deathImage.setScale(0.9);
+    deathImage.setAlpha(0);
+    
+    buttonStart = this.add.image(350, 350, 'button').setInteractive();
+    buttonStart.on('pointerdown', startGame);
 
     buttonImageHunger = this.add.image(117, 650, 'button').setInteractive();
     buttonImageHunger.setScale(0.3);
@@ -42,7 +59,7 @@ function create() {
     stateTextHunger = this.add.text(117, 600, "", 
         { fontFamily: 'Arial', fontSize: 18, color: '#00ff00' });
 
-    buttonImageSleep = this.add.image(350, 650, 'button').setInteractive();
+    buttonImageSleep = this.add.image(360, 650, 'button').setInteractive();
     buttonImageSleep.setScale(0.3);
     buttonImageSleep.tint = 0x1673F8;
     buttonImageSleep.on('pointerdown', sleep);
@@ -50,7 +67,7 @@ function create() {
     stateTextSleep = this.add.text(350, 600, "", 
         { fontFamily: 'Arial', fontSize: 18, color: '#00ff00' });
 
-    buttonImagePlay = this.add.image(580, 650, 'button').setInteractive();
+    buttonImagePlay = this.add.image(600, 650, 'button').setInteractive();
     buttonImagePlay.setScale(0.3);
     buttonImagePlay.tint = 0xffa500;
     buttonImagePlay.on('pointerdown', play);
@@ -64,6 +81,16 @@ function create() {
     updateState();
     setInterval(updateState, 10000);
     clickCounter = 0;
+
+    backgroundMusic = this.sound.add('backgroundMusic');
+    backgroundMusic.loop = true;
+    
+}
+
+function startGame(){
+    buttonStart.setAlpha(0);
+    backgroundMusic.play();
+    musicOn = true;
 }
 
 function updateState() {
@@ -74,6 +101,10 @@ function updateState() {
         }) 
             .then(function (stateFromServer) { 
                 updateCerberiusStats(stateFromServer);
+                if (musicOn == false){
+                    backgroundMusic.play();
+                    musicOn = true;
+                }
             }) 
     .catch(function (err) { 
         console.log("Something went wrong!", err); 
@@ -145,8 +176,8 @@ function counterManager(){
     }
 }
 
-function resetCounter(stateFromServer){
-    if (stateFromServer.death == false){
+function resetCounter(){
+    if (deathImage.alpha == 0){
         clickCounter = 0;
         stateTextCounter.text = "Counter : " + clickCounter;
         buttonImageHunger.setInteractive();
@@ -154,7 +185,7 @@ function resetCounter(stateFromServer){
         buttonImageSleep.setInteractive();
         buttonImageSleep.setAlpha(1);
         buttonImagePlay.setInteractive();
-        buttonImagePlay.setAlpha(1);   
+        buttonImagePlay.setAlpha(1);
     }
 }
 
@@ -162,7 +193,7 @@ function updateCerberiusStats(stateFromServer){
     stateTextHunger.text = "Hunger : " + stateFromServer.hunger;
     stateTextSleep.text = "Sleep : " + stateFromServer.sleep;
     stateTextPlay.text = "Mood : " + stateFromServer.mood;
-    if (death == true){
+    if (stateFromServer.death == true){
         //change asset Cerberius
         buttonImageHunger.disableInteractive();
         buttonImageHunger.setAlpha(0.5);
